17d14f022e17c39f193c55ed7123a81b2398395b
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-04-13 23:36:43 +0300

    Initial commit

---
+++ lib/debt.ex
@@ -0,0 +1,18 @@
+defmodule Debt do
+  @moduledoc """
+  Documentation for Debt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Debt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/debt_test.exs
@@ -0,0 +1,8 @@
+defmodule DebtTest do
+  use ExUnit.Case
+  doctest Debt
+
+  test "greets the world" do
+    assert Debt.hello() == :world
+  end
+end
