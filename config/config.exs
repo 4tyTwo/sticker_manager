6579164c09b1197689c041c07626c79f4b065485
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-10-19 16:25:48 +0300

    Delete proxy config and unused token_path

--- config/config.exs
+++ config/config.exs
@@ -4,14 +4,7 @@ use Mix.Config
 config :logger, backends: []
 
 config :nadia,
-  token: {:system, "BOT_TOKEN"},
-  proxy: {
-    :socks5,
-    to_charlist(System.get_env("PROXY_HOST")),
-    String.to_integer(System.get_env("PROXY_PORT"))
-  },
-  socks5_user: System.get_env("PROXY_USER"),
-  socks5_pass: System.get_env("PROXY_PASS")
+  token: {:system, "BOT_TOKEN"}
 
 config :debt, Debt.Scheduler,
   jobs: [
@@ -65,6 +58,5 @@ config :debt, :logger, [
 config :debt,
   # strictly 512x512 pixels!
   template_image_path: "./resources/images/sticker_template.png",
-  bot_token_path: "./resources/bot.token",
   user_id: String.to_integer(System.get_env("USER_ID")),
   secret: System.get_env("SECRET")
